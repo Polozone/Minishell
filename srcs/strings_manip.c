a9fae75ee98292e89631a031311db6f8271cd0f6
Maxime Golinvaux <mgolinva@student.42lyon.fr>
2022-09-14 08:57:03 +0200

    strncmp enleve

--- srcs/strings_manip.c
+++ srcs/strings_manip.c
@@ -6,7 +6,7 @@
 /*   By: mgolinva <mgolinva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 11:26:43 by mgolinva          #+#    #+#             */
-/*   Updated: 2022/07/25 16:05:56 by mgolinva         ###   ########.fr       */
+/*   Updated: 2022/09/14 08:54:46 by mgolinva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -178,23 +178,6 @@ void	ft_free_char_array(char **array)
 		i ++;
 	}
 	free(array);
-}
-
-
-
-int ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-    size_t  i;
-
-    i = 0;
-    while ((((unsigned char *)s1)[i] || ((unsigned char *)s2)[i]) && i < n)
-    {
-        if (((unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
-            i++;
-        else
-            return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-    }
-    return (0);
 }
 
 /*void	ft_free_array(void **array, int len)
